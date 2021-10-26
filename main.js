6ed5d09b939506669fb908cd36cda4c030c7b5ef
zzzdnwo <swjdnwo@gmail.com>
2021-10-26 16:05:45 +0900

    remarkadd

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
 
-	let ulLength = document.querySelectorAll('ul').length;
-	if(ulLength >3){
+	//메뉴 클릭 했을 때마다 if 쳐주는걸로 해야하나?
+	let ulLength = document.querySelectorAll('ul li').length;
+
+	if(ulLength>3){
 		console.log(ulLength);
-		$('#up').hide();
+		$('#up').show();
 		$('#down').show();
 	}else{
 		console.log(ulLength);
-	}
+		$('#up').hide();
+		$('#down').hide();
+	};
+
 	$('#up').click(function(){
 		$('div ul li:last-child').insertBefore(('div ul li:first-child'));
 		//$('div ul').css('margin-top',114);
